7a6c9a828f07c340fafb72f9888ccf72d019080a
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-10 19:05:36 +0300

    https://www.patika.dev/tr

--- src/Sort.java
+++ src/Sort.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Sort {
+import java.util.Scanner;
+
+public class Sort {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1. sayıyı giriniz");
+        a = scanner.nextInt();
+        System.out.println("2. sayıyı giriniz");
+        b = scanner.nextInt();
+        System.out.println("3. sayıyı giriniz");
+        c = scanner.nextInt();
+
+        if ((a < b) && (a < c)) {
+            if (b < c) {
+                System.out.println("a<b<c");
+            } else {
+                System.out.println("a<c<b");
+            }
+        }
+        if ((b < a) && (b < c)) {
+            if (a < c) {
+                System.out.println("b<a<c");
+            } else {
+                System.out.println("b<c<a");
+            }
+        }
+        if ((c < a) && (c < b)) {
+            if (a < b) {
+                System.out.println("c<a<b");
+            } else {
+                System.out.println("c<b<a");
+            }
+        }
+
+    }
 }
